b619805070c6da3c575d4b26e09d1418125ba245
sostennyirenda <sostennyirenda@gmail.com>
2022-05-09 16:46:42 +0200

    completed the functionality of the order screen

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import Axios from 'axios';
 import React, { useContext, useEffect, useReducer } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { Store } from '../Store';
 
 const reducer = (state, action) => {
@@ -8,7 +8,7 @@ const reducer = (state, action) => {
     case 'FETCH_REQUEST':
       return {...state, loading: true};
     case 'FETCH_SUCCESS':
-      return {...state, loading: false, error: false, data: action.payload};
+      return {...state, loading: false, error: false, order: action.payload};
     case 'FETCH_FAIL':
       return {...state, loading: false, error: action.payload}
     default:
@@ -17,24 +17,25 @@ const reducer = (state, action) => {
 }
 
 const OrderScreen = () => {
-
-const { orderId } = useParams;
+const params = useParams();
+const { id: orderId } = params;
 
 const navigate = useNavigate();
 const { state } = useContext(Store);
 const { userInfo } = state;
 
 const [{loading, error, order}, dispatch] = useReducer(reducer, {
+  order: {},
   loading: true,
   error: '',
-  order: {}
+  
 });
 
 useEffect(() => {
   const fetchData = async () => {
     try {
       dispatch ({type: 'FETCH_REQUEST'});
-      const { data } = await axios.get(`/api/order/${orderId}`, {
+      const { data } = await Axios.get(`/api/order/${orderId}`, {
         headers: { authorization: `Bearer ${userInfo.token}` }
       })
       dispatch({type: 'FETCH_SUCCESS', payload: data})
@@ -57,11 +58,67 @@ useEffect(() => {
       {loading ? (<div>Loading...</div>):
       error ? (<div>{error}</div>):(
         <div>
-          <h1>Order {orderId}</h1>
+          <h1>Order ID: {orderId}</h1>
+          <hr />
+          <div>
+            <h3>Shipping</h3>
+            <p><strong>Name:</strong > {order.shippingAddress.fullName}</p>
+            <p><strong>Address:</strong> {order.shippingAddress.address}, 
+            {order.shippingAddress.city}, {order.shippingAddress.country},
+            {order.shippingAddress.zipCode}</p>
+            <hr />
+            {order.isDelivered ? (
+              <div>
+                <p>Delivered at: {order.deliveredAt}</p>
+                <hr />
+              </div>
+            ):(
+              <div>
+                <p>Not delivered</p>
+                <hr />
+              </div>
+            )
+          }
+            <h3>Payment</h3>
+            <p><strong>Method:</strong> {order.paymentMethod}</p> 
+            <hr />
+            <div>
+              {order.isPaid ? (
+                <div>
+                  <p><strong>Paid At:</strong> {order.paidAt}</p>
+                  <hr />
+                </div>
+              ):(
+                <div>
+                  <p>Not Paid</p>
+                  <hr />
+                </div>
+              )}
+
+              <div>
+                <h3>items</h3>
+                {
+                  order.orderItems.map((item)=>(
+                    <div key={item._id}>
+                      <p><img src={item.image} alt={item.productName} /></p>
+                      <p>Name: {item.productName}</p>
+                      <p>Qty: {item.quantity}</p>
+                      <p>Price: ${item.price}</p>
+                      <Link to={`/product/${item.slug}`}>Edit</Link>
+                      <hr />
+                    </div>
+                  ))
+                }
+              </div>
+              <div>
+                <h3>Order Summary</h3>
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </div>
   )
 }
 
-export default OrderScreen
+export default OrderScreen;
